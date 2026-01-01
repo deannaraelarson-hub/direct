9382d13dbe5d37d38fb7f0c0de0376761b45396d
deannaraelarson-hub <deannaraelarson@gmail.com>
2026-01-01 17:19:10 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,22 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
-  },
   build: {
-    outDir: 'dist',
-    sourcemap: false,
-    emptyOutDir: true
-  },
-  define: {
-    'process.env': {},
-    'global': {}
-  },
-  resolve: {
-    alias: {
-      buffer: 'buffer/'
-    }
+    outDir: 'dist'
   }
 })

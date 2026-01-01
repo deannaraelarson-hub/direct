860cf36d7b53fa75374c6d7aacd390b446f7c2bb
deannaraelarson-hub <deannaraelarson@gmail.com>
2026-01-01 17:18:15 +0100

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,8 +1,6 @@
-// main.jsx - React entry point
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
